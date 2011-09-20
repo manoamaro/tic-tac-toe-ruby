ef01430e753e57d6d51e79cd562157e2b981f0ee
Manoel de Albuquerque Lira Amaro <mano.lira@gmail.com>
2011-09-20 15:46:52 -0300

    Tests in evaluate function

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,18 +1,22 @@
+#encoding:UTF-8
 require './ab_tree.rb'
 
 PLAYER1 = 1
 PLAYER2 = 2
 WIN = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-player1 = [0,1,3]
-player2 = [0,3,6]
+player1 = []
+player2 = []
 
 def evaluate(p1,p2)
+  if p1.empty? or p2.empty?
+    return 0
+  end
   WIN.each do |test|
-    if p1 & test == test
+    if (test - p1).empty?
       return 1
     end
-    if p2 & test == test
+    if (test - p2).empty?
       return -1
     end
   end
@@ -48,5 +52,28 @@ def alphabeta(node, depth, alpha, beta, player)
       end
     end
     return beta
+  end
+end
+
+playing_now = 1
+
+while(true)
+  if playing_now == 1
+    puts "Player 1, digite a posição: "
+    p = gets
+    player1 << p.to_i
+    playing_now = 2
+  else
+    puts "Player 2, digite a posição: "
+    p = gets
+    player2 << p.to_i
+    playing_now = 1
+  end
+  resultado = evaluate(player1,player2)
+  puts "\nResultado: #{evaluate(player1,player2)}\n"
+  if (resultado == 1 || resultado == 2 || player1.size + player2.size == 9)
+    puts "Player 1: #{player1}"
+    puts "Player 2: #{player2}"
+    break
   end
 end
